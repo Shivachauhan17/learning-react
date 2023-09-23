8689d22ce3016bc5124f627181e888b612cb877f
Shivachauhan17 <shivanbd2019@gmail.com>
2023-09-23 20:26:48 +0530

    completed react fundamentals

--- my-react-app/src/components/performance/03-hooks/Person.jsx
+++ my-react-app/src/components/performance/03-hooks/Person.jsx
@@ -1,8 +1,9 @@
-const Person = ({ name }) => {
+const Person = ({ name ,removePerson,id}) => {
   console.log('in person')
     return (
       <div>
         <h4>{name}</h4>
+        <button onClick={()=>removePerson(id)}>remove</button>
       </div>
     );
   };
